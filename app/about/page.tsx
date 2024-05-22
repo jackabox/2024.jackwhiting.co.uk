45b5aa9aaabf0f66e6ce8d91d59097275033bc2d
Jack Whiting <hi@jackwhiting.co.uk>
2024-05-22 01:55:32 +0100

    basic set up for about

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,4 +1,5 @@
 import { getApiQuery } from "@/utils/getApiQuery"
+import Image from "next/image"
 
 async function getData() {
   const data = await getApiQuery()
@@ -9,7 +10,22 @@ async function getData() {
 const Page = async () => {
   const data = await getData()
 
-  return <></>
+  return (
+    <>
+      <div className="grid grid-cols-2 gap-48 container py-24">
+        <Image src="/me.jpg" alt="Picture of Jack" width="800" height="900" />
+
+        <div className="pt-6 max-w-[600px]">
+          <h2 className="mb-6">I'm Jack</h2>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam earum
+            nihil repudiandae vitae provident. Saepe laborum, aliquid ratione
+            veniam distinctio!
+          </p>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default Page
--- components/Projects/ProjectFullRow.tsx
+++ components/Projects/ProjectFullRow.tsx
@@ -7,105 +7,105 @@ import ReactPlayer from "react-player/vimeo"
 import SimpleButton from "../SimpleButton"
 
 function VideoComponent({ url }: any) {
-  return (
-    <div className="rounded-lg overflow-hidden iframe-square h-full">
-      <ReactPlayer
-        width="100%"
-        height="100%"
-        muted
-        playsinline
-        loop
-        playing
-        url={url}
-      />
-    </div>
-  )
+	return (
+		<div className="rounded-lg overflow-hidden iframe-square h-full">
+			<ReactPlayer
+				width="100%"
+				height="100%"
+				muted
+				playsinline
+				loop
+				playing
+				url={url}
+			/>
+		</div>
+	)
 }
 
 export default function ProjectFullRow({
-  title,
-  url,
-  projectType,
-  image,
-  summary,
-  video,
+	title,
+	url,
+	projectType,
+	image,
+	summary,
+	video,
 }: any) {
-  return (
-    <Link href={url} className="group  even:mt-48">
-      <AnimatePresence>
-        {image.length && (
-          <div className="shadow-2xl overflow-hidden">
-            <motion.div
-              initial="initial"
-              animate="animated"
-              exit="initial"
-              variants={{
-                initial: {
-                  scale: 1.15,
-                  transition: {
-                    duration: 0,
-                  },
-                },
-                animated: {
-                  scale: 1,
-                  transition: {
-                    duration: 1,
-                  },
-                },
-              }}
-            >
-              <div className="p-16 bg-blue aspect-[9/8]">
-                {video ? (
-                  <VideoComponent url={video} />
-                ) : (
-                  <Image
-                    src={image[0].url}
-                    width={image[0].width}
-                    height={image[0].height}
-                    alt={image[0].alt}
-                  />
-                )}
-              </div>
-            </motion.div>
-          </div>
-        )}
-      </AnimatePresence>
+	return (
+		<Link href={url} className="group  even:mt-48">
+			<AnimatePresence>
+				{image.length && (
+					<div className="shadow-2xl overflow-hidden">
+						<motion.div
+							initial="initial"
+							animate="animated"
+							exit="initial"
+							variants={{
+								initial: {
+									scale: 1.15,
+									transition: {
+										duration: 0,
+									},
+								},
+								animated: {
+									scale: 1,
+									transition: {
+										duration: 1,
+									},
+								},
+							}}
+						>
+							<div className="p-16 bg-blue aspect-[9/8]">
+								{video ? (
+									<VideoComponent url={video} />
+								) : (
+									<Image
+										src={image[0].url}
+										width={image[0].width}
+										height={image[0].height}
+										alt={image[0].alt}
+									/>
+								)}
+							</div>
+						</motion.div>
+					</div>
+				)}
+			</AnimatePresence>
 
-      <div className="mt-10 w-auto flex gap-4 flex-wrap mb-7">
-        {projectType.map((type: any, index: number) => {
-          return (
-            <span
-              key={index}
-              className="flex text-sm font-mono uppercase font-regular px-2 py-1 rounded-md border border-black opacity-50"
-            >
-              {type.title}
-            </span>
-          )
-        })}
-      </div>
+			<div className="mt-10 w-auto flex gap-4 flex-wrap mb-7">
+				{projectType.map((type: any, index: number) => {
+					return (
+						<span
+							key={index}
+							className="flex text-sm font-mono uppercase font-regular px-2 py-1 rounded-md border border-black opacity-50"
+						>
+							{type.title}
+						</span>
+					)
+				})}
+			</div>
 
-      <div className="flex items-center gap-2">
-        <h2 className="font-normal transition-all duration-300 group-hover:text-blue">
-          {title}
-        </h2>
+			<div className="flex items-center gap-2">
+				<h3 className="font-normal transition-all duration-300 group-hover:text-blue">
+					{title}
+				</h3>
 
-        <span className="opacity-0 group-hover:opacity-100 transition-opacity duration-500 text-blue">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            className="w-6 h-6"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3"
-            />
-          </svg>
-        </span>
-      </div>
-    </Link>
-  )
+				<span className="opacity-0 group-hover:opacity-100 transition-opacity duration-500 text-blue">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke-width="1.5"
+						stroke="currentColor"
+						className="w-6 h-6"
+					>
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3"
+						/>
+					</svg>
+				</span>
+			</div>
+		</Link>
+	)
 }
