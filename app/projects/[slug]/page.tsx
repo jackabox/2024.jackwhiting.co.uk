c3820afbfe9c2110f5951b1fcb02a516881d34bf
Jack Whiting <hi@jackwhiting.co.uk>
2024-06-07 03:43:21 +0100

    updating page for projects

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -2,6 +2,7 @@ import { getApiQuery } from "@/utils/getApiQuery.js"
 import { SINGLE_PROJECT_QUERY } from "@/queries/projects"
 import FullVideo from "@/components/Projects/FullVideo"
 import SimpleButton from "@/components/SimpleButton"
+import Link from "next/link"
 
 async function getData(params: any) {
   const { projectsEntries } = await getApiQuery(SINGLE_PROJECT_QUERY, params)
@@ -26,35 +27,15 @@ export default async function Page({ params }: { params: { slug: string } }) {
 
   return (
     <>
-      <div className="py-24 lg:py-32 container">
-        <p className="uppercase font-mono text-semibold tracking-wider small text-black/40">
+      <section className="container py-24 lg:pb-48 text-center">
+        <p className="mb-8 uppercase font-mono text-semibold tracking-wider  text-black/40">
           Projects
         </p>
 
-        <div className="mt-4 flex justify-between items-center">
-          <h1 className=" leading-tight font-normal">{title}</h1>
-
-          <a
-            href={websiteUrl}
-            target="_blank"
-            title="Visit website for this project"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              className="w-16 h-16"
-              fill="none"
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="1"
-            >
-              <polyline points="10 9 15 4 20 9" />
-              <path d="M4 20h7a4 4 0 0 0 4-4V4" />
-            </svg>
-          </a>
-        </div>
-      </div>
+        <h1 className="leading-tight font-normal h1 max-w-[1300px] mx-auto">
+          {title}
+        </h1>
+      </section>
 
       <div className="py-24 bg-blue">
         <div className="container">
@@ -70,12 +51,14 @@ export default async function Page({ params }: { params: { slug: string } }) {
             dangerouslySetInnerHTML={{ __html: overview }}
           />
 
-          <SimpleButton
-            url={websiteUrl}
-            title="Visit website for this project"
-            text="Visit Website"
-            className="mt-10"
-          />
+          {websiteUrl && (
+            <SimpleButton
+              url={websiteUrl}
+              title="Visit website for this project"
+              text="Visit Website"
+              className="mt-10"
+            />
+          )}
         </div>
 
         <div className="w-1/3 grid grid-cols-2 gap-20">
